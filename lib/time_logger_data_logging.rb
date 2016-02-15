7e0264792e5ca9b2ea75003b77b8fe850c27ff7a
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-02-15 10:54:49 -0600

    More tests, refactoring

--- lib/time_logger_data_logging.rb
+++ lib/time_logger_data_logging.rb
@@ -5,15 +5,23 @@ class TimeLoggerDataLogging
     @time_log_file_name = filename
   end
 
-  def log_time(date, hours, timecode)
+  def log_time(args = {})
+    date = args[:date]
+    hours = args[:hours]
+    timecode = args[:timecode]
+    client = args.fetch(:client, nil)
     CSV.open(@time_log_file_name, "ab") do |csv|
-      csv << [date, hours, timecode]
+      csv << [date, hours, timecode, client]
     end
+  end
+
+  def read_data
+    CSV.read(@time_log_file_name)
   end
 
 
   def clear_data
-    CSV.open(@time_log_file_name, "wb") do |csv|
+    CSV.open(@time_log_file_name, "w") do |csv|
     end
   end
 
--- spec/time_logger_data_logging_spec.rb
+++ spec/time_logger_data_logging_spec.rb
@@ -2,6 +2,23 @@ require "time_logger_data_logging"
 
 describe TimeLoggerDataLogging do
 
+  def no_client_log
+    date = "2/15/2016"
+    hours = "4"
+    timecode = "PTO"
+    @logger.log_time(date: date, hours: hours, timecode: timecode)
+    [date, hours, timecode, nil]
+  end
+
+  def client_log
+    date = "2/10/2016"
+    hours = "2"
+    timecode = "Billable Work"
+    client = "Bob"
+    @logger.log_time(date: date, hours: hours, timecode: timecode, client: client)
+    [date, hours, timecode, client]
+  end
+
   before do
     @file_name = "tester.csv"
     @logger = TimeLoggerDataLogging.new(@file_name)
@@ -9,12 +26,22 @@ describe TimeLoggerDataLogging do
   end
 
   it "can save date, hours, and timecode to default file" do
-    date = "2/15/2016"
-    hours = "4"
-    timecode = "Billable Work"
-    @logger.log_time(date, hours, timecode)
+    recorded_data = no_client_log
     data = CSV.read(@file_name)
-    expect(data[0]).to eql [date, hours, timecode]
+    expect(data[0]).to eql recorded_data
+  end
+
+  it "can save date, hours, timecode, and client to default file" do
+    recorded_data = client_log
+    data = CSV.read(@file_name)
+    expect(data[0]).to eql recorded_data
+  end
+
+  it "can retrieve array of all rows" do
+    no_client = no_client_log
+    client = client_log
+    data = @logger.read_data
+    expect(data).to eql [no_client, client]
   end
 
 end
